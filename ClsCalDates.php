8100fdd7c9917aae9b0307f9ea1c29941dca7535
totorosun <393878249@qq.com>
2017-06-10 00:30:00 +0800

    Add files via upload

---
+++ ClsCalDates.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/6/9
+ * Time: 16:00
+ */
+
+namespace ruanjian;
+
+
+class ClsCalDates
+{
+    static function getMonthDay($date)
+    {
+        $firstday = date('Y-m-01', strtotime($date));
+        $lastday = date('Y-m-d', strtotime("$firstday +1 month -1 day"));
+        return $lastday - $firstday + 1;
+    }
+}
