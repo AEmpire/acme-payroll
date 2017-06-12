9c7c0a00058100003dcf2f094727bae077323f69
totorosun <393878249@qq.com>
2017-06-12 14:56:48 +0800

    新风格界面

--- Administrator/menu.php
+++ Administrator/menu.php
@@ -18,6 +18,20 @@ $clsDefault->resetCookie();
 
 <head>
 <title>Main Menu</title>
+
+
+    <!-- Bootstrap Styles-->
+    <link href="../css/bootstrap.css" rel="stylesheet" />
+    <!-- FontAwesome Styles-->
+    <link href="../css/font-awesome.css" rel="stylesheet" />
+    <!-- Morris Chart Styles-->
+    <link href="../js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <!-- Custom Styles-->
+    <link href="../css/custom-styles.css" rel="stylesheet" />
+    <!-- Google Fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <link rel="stylesheet" href="../js/Lightweight-Chart/cssCharts.css">
+
 </head>
 <body>
 <h1 align="center">
